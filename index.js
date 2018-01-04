e97cdaee2ae21d34e24aa91145b46f304dc3a61d
Edward Norton <edwardnorton@icloud.com>
2018-01-04 23:42:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,34 @@ function addToCart(item) {
 }
 
 function viewCart() {
-  
+  const l = cart.length;
+
+  if (!l) {
+    return console.log("Your shopping cart is empty.");
+  }
+
+  let itemsAndPrices = [];
+
+  for (let i = 0; i < l; i++) {
+    let itemAndPrice = cart[i];
+    let item = Object.keys(itemAndPrice)[0];
+    let price = itemAndPrice[item];
+
+    itemsAndPrices.push(`${item} at \$${price}`);
+  }
+
+  switch(itemsAndPrices.length) {
+    case 1:
+      break;
+    case 2:
+      itemsAndPrices = itemsAndPrices.join(" and ");
+      break;
+    default:
+      itemsAndPrices[l-1] = "and ".concat(itemsAndPrices[l-1]);
+      itemsAndPrices = itemsAndPrices.join(", ");
+  }
+
+  console.log(`In your cart, you have ${itemsAndPrices}.`);
 }
 
 function total() {
